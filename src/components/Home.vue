848153845e82b0a0f60730a5370b02e1df092401
Felipe Guimarães <felipeemg@gmail.com>
2019-06-22 09:43:04 -0300

    Vue with Vuetify

---
+++ src/components/Home.vue
@@ -0,0 +1,41 @@
+<template>
+
+  <div class="container">
+    <Menu />
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+import Menu from './Menu'
+
+export default {
+  name: 'ParkingSpot',
+  components: {
+    Menu
+  },
+  data () {
+    return {
+      msg: 'Parking Spot'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
